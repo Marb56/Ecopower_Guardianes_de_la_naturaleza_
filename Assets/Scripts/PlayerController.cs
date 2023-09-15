ed641b758ca5c675a2fb710c38fa0df706a1b6a0
MiguelR <Miguelr_56@hotmail.com>
2023-09-14 23:30:39 -0500

    Avanze_14_09_2023
    Avance ecopower_Nivel_1_y_Personaje

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale = 5f;
+
+    public float rotateSpeed = 5f;
+
+    private Vector3 moveDirection;
+
+    public CharacterController charController;
+    public Camera playerCamera;
+    public GameObject playerModel;
+
+    public Animator animator;
+
+    private int jumpsPerformed = 0;
+    public int maxJumps = 2; // Número máximo de saltos permitidos, incluyendo el salto inicial.
+
+    void Update()
+    {
+        float yStore = moveDirection.y;
+
+        // Movimiento
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
+
+        // Salto
+        if (charController.isGrounded)
+        {
+            // Reinicia el número de saltos realizados cuando tocas el suelo.
+            jumpsPerformed = 0;
+
+            moveDirection.y = 0f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+                jumpsPerformed++;
+            }
+        }
+        else
+        {
+            // Si no estás en el suelo y aún tienes saltos disponibles, permite el doble salto.
+            if (Input.GetButtonDown("Jump") && jumpsPerformed < maxJumps - 1)
+            {
+                moveDirection.y = jumpForce;
+                jumpsPerformed++;
+            }
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        charController.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+        animator.SetBool("Grounded", charController.isGrounded);
+    }
+}
